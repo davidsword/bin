20099e99b83557c7627936bee273d69e64ffae48
David Sword <david@davidsword.ca>
2021-03-08 13:24:54 -0800

    add config sample

---
+++ _config-sample.php
@@ -0,0 +1,10 @@
+<?php
+
+define('SLACKTOKEN', '');
+define('STATUSES_JSON', '' );
+
+define( 'HOME_ASSISTANT_TOKEN', '' );
+define( 'HOME_ASSISTANT_HOST_URL', '' );
+
+define( 'HOME_ASSISTANT_BUSYLIGHT_ENTITY_FRIENDLY', '');
+define( 'HOME_ASSISTANT_BUSYLIGHT_ENTITY', '');
